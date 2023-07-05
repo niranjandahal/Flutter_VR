94c2abfd5a201f0915e388719c188ab467c1ae3c
Niranjan Dahal <niranjandahal76@gmail.com>
2023-07-05 17:32:07 +0545

    changes

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:r06placevr/placedatabasesection/placehomesection.dart';
-import 'package:r06placevr/vrsection/vrdetailscreen.dart';
+import 'package:r06placevr/vrsection/vrhomesection.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
@@ -11,54 +10,13 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
-  final List<dynamic> places = [
-    {
-      "name": "Pashupatinath Temple",
-      "photo": "assets/Pashupatinath.jpg",
-      "vrvideoLink": "-h9dh8BzUqM"
-    },
-    {
-      "name": "Pokhara",
-      "photo": "assets/Pokhara.jpg",
-      "vrvideoLink": "rpCjeuDBD4M"
-    },
-    {
-      "name": "Boudhanath Stupa",
-      "photo": "assets/Boudhanath Stupa.jpg",
-      "vrvideoLink": "2FnS_W3XpC4"
-    },
-    {
-      "name": "Everest Base Camp",
-      "photo": "assets/Everestcamp.jpg",
-      "vrvideoLink": "nqC7VnbCY7k"
-    },
-    {
-      "name": "Durbar Square",
-      "photo": "assets/Durbarsquare.jpg",
-      "vrvideoLink": "ASZlMNWev7s"
-    },
-    {
-      "name": "Garden of Dreams",
-      "photo": "assets/Gardenofdreams.jpg",
-      "vrvideoLink": "msvnJVLuZds"
-    },
-  ];
-
-  int _selectedIndex = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp, 
-    ]);
-  }
-
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
   }
+
+  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -67,66 +25,7 @@ class _HomepageState extends State<Homepage> {
         index: _selectedIndex,
         children: [
           // VR section
-          GridView.builder(
-            physics: BouncingScrollPhysics(),
-            padding: EdgeInsets.all(16),
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 16,
-              mainAxisSpacing: 16,
-              childAspectRatio: 0.8,
-            ),
-            itemCount: places.length,
-            itemBuilder: (context, index) {
-              final place = places[index];
-              return GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => VrDetailsSection(
-                        placename: place['name'],
-                        videoid: place['vrvideoLink'],
-                      ),
-                    ),
-                  );
-                },
-                child: Card(
-                  elevation: 4,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Expanded(
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.vertical(
-                            top: Radius.circular(8),
-                          ),
-                          child: Image.asset(
-                            place['photo'],
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(8),
-                        child: Text(
-                          place['name'],
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            },
-          ),
+          VrHomeSection(),
 
           // Home section
           PlaceHomeSection(),
@@ -139,8 +38,8 @@ class _HomepageState extends State<Homepage> {
             label: 'VR',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
+            icon: Icon(Icons.place),
+            label: 'places',
           ),
         ],
         currentIndex: _selectedIndex,
--- lib/vrsection/vrhomesection.dart
+++ lib/vrsection/vrhomesection.dart
@@ -1,22 +1,54 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:youtube_explode_dart/youtube_explode_dart.dart';
+import 'package:flutter/services.dart';
+import 'package:r06placevr/vrsection/vrdetailscreen.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class VrHomeSection extends StatefulWidget {
+  const VrHomeSection({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  State<VrHomeSection> createState() => _VrHomeSectionState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  var yt = YoutubeExplode();
-
-  Future<void> youtubeexplode() async {}
+class _VrHomeSectionState extends State<VrHomeSection> {
+  final List<dynamic> places = [
+    {
+      "name": "Pashupatinath Temple",
+      "photo": "assets/Pashupatinath.jpg",
+      "vrvideoLink": "-h9dh8BzUqM"
+    },
+    {
+      "name": "Pokhara",
+      "photo": "assets/Pokhara.jpg",
+      "vrvideoLink": "rpCjeuDBD4M"
+    },
+    {
+      "name": "Boudhanath Stupa",
+      "photo": "assets/Boudhanath Stupa.jpg",
+      "vrvideoLink": "2FnS_W3XpC4"
+    },
+    {
+      "name": "Everest Base Camp",
+      "photo": "assets/Everestcamp.jpg",
+      "vrvideoLink": "nqC7VnbCY7k"
+    },
+    {
+      "name": "Durbar Square",
+      "photo": "assets/Durbarsquare.jpg",
+      "vrvideoLink": "ASZlMNWev7s"
+    },
+    {
+      "name": "Garden of Dreams",
+      "photo": "assets/Gardenofdreams.jpg",
+      "vrvideoLink": "msvnJVLuZds"
+    },
+  ];
 
   @override
   void initState() {
     super.initState();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
   }
 
   @override
@@ -25,10 +57,66 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: Text('Vr Home Section'),
       ),
-      body: Center(
-          child: Column(
-        children: [],
-      )),
+      body: GridView.builder(
+        physics: BouncingScrollPhysics(),
+        padding: EdgeInsets.all(16),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 16,
+          mainAxisSpacing: 16,
+          childAspectRatio: 0.8,
+        ),
+        itemCount: places.length,
+        itemBuilder: (context, index) {
+          final place = places[index];
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => VrDetailsSection(
+                    placename: place['name'],
+                    videoid: place['vrvideoLink'],
+                  ),
+                ),
+              );
+            },
+            child: Card(
+              elevation: 4,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(8),
+                      ),
+                      child: Image.asset(
+                        place['photo'],
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Text(
+                      place['name'],
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
